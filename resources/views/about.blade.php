9275d1687e3fbb6a1e367795cad95dc5db735fce
shan5742 <asamshan456@gmail.com>
2018-11-13 20:03:48 +0000

    adding jumbotron and some styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
   <h1>Aobut</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, at velit magni aspernatur et dignissimos eligendi facere ducimus corporis ab perspiciatis quia reprehenderit vel laboriosam voluptatum. Autem ipsum quasi illo?
+  </p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
   <h1>Home</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa optio facere dicta? Quisquam dolores delectus possimus eum exercitationem reiciendis! Eum, at? Obcaecati quia earum asperiores reprehenderit cum ipsam reiciendis modi.</p>
 @endsection
 
 @section('sidebar')
+  @parent
   <p>This is appended to the sidebar</p>
 @endsection
